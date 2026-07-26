cca3ecc5c18e5b2d712ff0bec05532c5932466ab
Jyotirmay-jtp <joytoursindia7@gmail.com>
2026-07-27 01:19:50 +0530

    Create loading.tsx

---
+++ app/loading.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Loading() {
+  return (
+    <div className="fixed inset-0 z-[999] flex items-center justify-center bg-[#FAFAF7]">
+      <Image src="/images/logo.png" alt="Loading" width={80} height={80} className="animate-pulse rounded-full" />
+    </div>
+  );
+}
